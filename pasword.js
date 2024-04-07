07cea17f641627a7d1e481f65ed42cfcf7411713
Eazybel <eazybel27@gmail.com>
2024-04-07 14:00:18 +0300

    "Split character sets into separate arrays and randomly select elements to generate a password; display password and updated paragraph in HTML"
    This commit includes changes to both index.html and pasword.js. In index.html, a new paragraph element with the id "para1" has been added. In pasword.js, the lower, upper, numeric, and symbol constants have been split into separate arrays (wordslow, wordsup, wordsnum, and wordssym). A new array called passwrod has also been created to store the randomly selected characters. The onclick function for the button has been updated to populate the passwrod array with random elements from the character arrays and display the resulting password in the "para" paragraph element. Additionally, a new paragraph element with the id "para1" has been created and populated with an empty array called "paraa".

--- pasword.js
+++ pasword.js
@@ -4,9 +4,25 @@ const lower = "abcdefghijklmnopqrstuvwxyz";
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numeric = "0123456789";
 const symbol = "!@#$%^&*()-_+={}[]|\;:'`~;'";
-const words = lower.split("")
+const wordslow = lower.split("");
+const wordsup = upper.split("");
+const wordsnum = numeric.split("");
+const wordssym = symbol.split("");
+const passwrod = [];
+const para1=document.getElementById("para1");
+let paraa=[];
 btn.onclick=function(){
     
-    para.textContent= words[2];
+   passwrod[0]= wordslow[Math.floor(Math.random(10)*27)];
+   passwrod[1]= wordsup[Math.floor(Math.random(10)*27)];¬
+   passwrod[2]= wordsnum[Math.floor(Math.random(10)*10)];
+   passwrod[3]= wordssym[Math.floor(Math.random(10)*28)];
+   passwrod[4]= wordslow[Math.floor(Math.random(10)*27)];
+   passwrod[5]= wordsup[Math.floor(Math.random(10)*27)];
+   passwrod[6]= wordsnum[Math.floor(Math.random(10)*10)];
+   passwrod[7]= wordssym[Math.floor(Math.random(10)*27)];
+
+   para.textContent=passwrod.join("");
+   para1.textContent=paraa.join("");
 }
 
